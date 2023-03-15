c2260f74658e354a051f655c43283693ad9b2dfe
edemuner <eduardood15@hotmail.com>
2023-03-15 10:32:43 -0300

    testing input fields

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -6,10 +6,14 @@ describe('Create a New Item', () => {
     });
 
     it('should have a form', () => {
-        cy.get('form').should('exist')
+        cy.get('form')
     });
 
     it('should have the words "Add Item"', () => {
-        cy.contains('Add Item')
+        cy.contains('Add Item');
+    });
+
+    it('should put stuff in an input field', () => {
+        cy.get('[data-test="new-item-input"]').type('Good attitude')
     })
 });
