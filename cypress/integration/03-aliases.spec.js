3f15a93ce60da11acd1b7a017d7648d0532c96cb
edemuner <eduardood15@hotmail.com>
2023-03-21 11:28:22 -0300

    aliases

--- cypress/integration/03-aliases.spec.js
+++ cypress/integration/03-aliases.spec.js
@@ -4,4 +4,45 @@ describe('Aliases', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
   });
+
+  const item = 'Good attitude'
+
+  it('defines the aliases', () => {
+    
+  })
+  
+
+  it('should show items that match whatever is in the filter field', () => {
+    cy.get('[data-test="new-item-input"]').as('newItemBar')
+    cy.get('[data-test="add-item"]').as('addButton')
+    cy.get('[data-test="filter-items"]').as('filterBar')
+    cy.get('[data-test="items"] li').as('allItems')
+    cy.get('[data-test="items-unpacked"]').as('unpackedItems')
+
+    cy.get('@newItemBar').type(item)
+    cy.get('@addButton').click()
+    cy.get('@filterBar').type(item)
+    cy.get('@allItems').each(($item) => {
+      expect($item.text()).to.include(item)
+    });
+  });
+
+  it('should hide items that do not match whatever is in the filter field', () => {
+    cy.get('[data-test="new-item-input"]').as('newItemBar')
+    cy.get('[data-test="add-item"]').as('addButton')
+    cy.get('[data-test="filter-items"]').as('filterBar')
+    cy.get('[data-test="items"] li').as('allItems')
+    cy.get('[data-test="items-unpacked"]').as('unpackedItems')
+
+    cy.get('@newItemBar').type(item)
+    cy.get('@addButton').click()
+    cy.get('@newItemBar').type('other stuff')
+    cy.get('@addButton').click()
+    cy.get('@filterBar').type(item)
+    cy.get('@unpackedItems').should('not.contain', 'stuff');
+  });
+});
+
+describe('Filtering items with aliases', () => {
+  
 });
