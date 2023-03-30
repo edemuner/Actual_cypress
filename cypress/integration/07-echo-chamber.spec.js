1c0bc22e26f06b10ae1cfde5e001ac824add5ec8
edemuner <eduardood15@hotmail.com>
2023-03-30 10:47:12 -0300

    form validation

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -24,9 +24,20 @@ describe('Initial Page', () => {
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-submit"]').as('submit')
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submit').click();
+    cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validationMessage')
+      .should('contain', 'Please fill out this field')
+
+      cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true')
+  });
 
   it('should require that the email actually be an email address', () => {});
 
