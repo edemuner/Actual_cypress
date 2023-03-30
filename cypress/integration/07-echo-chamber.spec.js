3ef96cafd8e104430e3885882da06b8adc9dbefe
edemuner <eduardood15@hotmail.com>
2023-03-30 11:43:24 -0300

    form validation exercise

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -22,24 +22,62 @@ describe('Initial Page', () => {
 });
 
 describe('Sign Up', () => {
+  const invalidInput = "not-an-email"
+  const password = 'myPassword'
+  const validEmail = 'myEmail@server.com'
+
+
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
     cy.get('[data-test="sign-up-submit"]').as('submit')
+    cy.get('[data-test="sign-up-email"]').as('signupEmail')
+    cy.get('[data-test="sign-up-password"]').as('signupPassword')
   });
 
   it('should require an email', () => {
+
+    cy.get('@signupPassword').type(password)
     cy.get('@submit').click();
+
     cy.get('[data-test="sign-up-email"]:invalid')
       .invoke('prop', 'validationMessage')
       .should('contain', 'Please fill out this field')
 
-      cy.get('[data-test="sign-up-email"]:invalid')
+    cy.get('@signupEmail')
       .invoke('prop', 'validity')
       .its('valueMissing')
       .should('be.true')
   });
 
-  it('should require that the email actually be an email address', () => {});
+  it('should require that the email actually be an email address', () => {
 
-  it('should require a password when the email is present', () => {});
+    cy.get('@signupPassword').type(password)
+
+    cy.get('@signupEmail').type(invalidInput)
+    cy.get('@submit').click()
+
+    cy.get('@signupEmail')
+      .invoke('prop', 'validity')
+      .its('typeMismatch')
+      .should('be.true')
+
+    cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validationMessage')
+      .should('contain', `Please include an '@' in the email address. '${invalidInput}' is missing an '@'.`)
+  });
+
+  it('should require a password when the email is present', () => {
+
+    cy.get('@signupEmail').type(validEmail)
+    cy.get('@submit').click()
+
+    cy.get('[data-test="sign-up-password"]:invalid')
+      .invoke('prop', 'validationMessage')
+      .should('contain', 'Please fill out this field')
+
+    cy.get('@signupPassword')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true')
+  });
 });
