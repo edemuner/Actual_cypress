eefc262c7ceb86fc1201465170d9da8f6c1ed404
edemuner <eduardood15@hotmail.com>
2023-04-03 20:08:17 -0300

    verifying duplicated users on signup

--- cypress/integration/09-echo-chamber-with-tasks.spec.js
+++ cypress/integration/09-echo-chamber-with-tasks.spec.js
@@ -10,7 +10,7 @@ describe('Sign Up', () => {
   beforeEach(() => {
     cy.task('reset')
   })
-  it.only('should successfully create a user when entering an email and a password', () => {
+  it('should successfully create a user when entering an email and a password', () => {
     // Sign Up
     cy.visit('/echo-chamber/sign-up');
     cy.get('[data-test="sign-up-email"]').type(user.email);
@@ -28,11 +28,24 @@ describe('Sign Up', () => {
   });
 
   it('should successfully login with a provided seeded user', () => {
-    cy.task('reset')
-    cy.task('seed')
+    cy.task('reset');
+    cy.task('seed');
     cy.visit('/echo-chamber/sign-in');
     cy.get('[data-test="sign-in-email"]').type(user.email);
     cy.get('[data-test="sign-in-password"]').type(user.password);
     cy.get('[data-test="sign-in-submit"]').click();
+  })
+
+  it.only('should block duplicated users on signup', () => {
+    cy.task('reset')
+    cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-email"]').type(user.email);
+    cy.get('[data-test="sign-up-password"]').type(user.password);
+    cy.get('[data-test="sign-up-submit"]').click();
+    cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-email"]').type(user.email);
+    cy.get('[data-test="sign-up-password"]').type(user.password);
+    cy.get('[data-test="sign-up-submit"]').click();
+    cy.get('p').should('contain', 'A user already exists with that email.')
   })
 });
