578d8a8251e05b1640a415dd7e80b3cacd152cc6
edemuner <eduardood15@hotmail.com>
2023-04-04 12:04:35 -0300

    testing search

--- cypress/integration/09-echo-chamber-with-tasks.spec.js
+++ cypress/integration/09-echo-chamber-with-tasks.spec.js
@@ -12,16 +12,10 @@ describe('Sign Up', () => {
   })
   it('should successfully create a user when entering an email and a password', () => {
     // Sign Up
-    cy.visit('/echo-chamber/sign-up');
-    cy.get('[data-test="sign-up-email"]').type(user.email);
-    cy.get('[data-test="sign-up-password"]').type(user.password);
-    cy.get('[data-test="sign-up-submit"]').click();
+    cy.signUp(user)
 
     // Sign In
-    cy.visit('/echo-chamber/sign-in');
-    cy.get('[data-test="sign-in-email"]').type(user.email);
-    cy.get('[data-test="sign-in-password"]').type(user.password);
-    cy.get('[data-test="sign-in-submit"]').click();
+    cy.signIn(user)
 
     cy.location('pathname').should('contain', '/echo-chamber/posts');
     cy.contains('Signed in as ' + user.email);
@@ -30,22 +24,13 @@ describe('Sign Up', () => {
   it('should successfully login with a provided seeded user', () => {
     cy.task('reset');
     cy.task('seed');
-    cy.visit('/echo-chamber/sign-in');
-    cy.get('[data-test="sign-in-email"]').type(user.email);
-    cy.get('[data-test="sign-in-password"]').type(user.password);
-    cy.get('[data-test="sign-in-submit"]').click();
+    cy.signIn(user)
   })
 
-  it.only('should block duplicated users on signup', () => {
+  it('should block duplicated users on signup', () => {
     cy.task('reset')
-    cy.visit('/echo-chamber/sign-up');
-    cy.get('[data-test="sign-up-email"]').type(user.email);
-    cy.get('[data-test="sign-up-password"]').type(user.password);
-    cy.get('[data-test="sign-up-submit"]').click();
-    cy.visit('/echo-chamber/sign-up');
-    cy.get('[data-test="sign-up-email"]').type(user.email);
-    cy.get('[data-test="sign-up-password"]').type(user.password);
-    cy.get('[data-test="sign-up-submit"]').click();
-    cy.get('p').should('contain', 'A user already exists with that email.').log()
+    cy.signUp(user)
+    cy.signUp(user)
+    cy.get('p').should('contain', 'A user already exists with that email.')
   })
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('signUp' ,(user) => {
+    cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-email"]').type(user.email);
+    cy.get('[data-test="sign-up-password"]').type(user.password);
+    cy.get('[data-test="sign-up-submit"]').click();
+});
+
+Cypress.Commands.add('signIn' ,(user) => {
+    cy.visit('/echo-chamber/sign-in');
+    cy.get('[data-test="sign-in-email"]').type(user.email);
+    cy.get('[data-test="sign-in-password"]').type(user.password);
+    cy.get('[data-test="sign-in-submit"]').click();
+});
